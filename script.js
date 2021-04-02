2448572e60cb96c327fa4601685077bfb646ebe6
moacircvicente <cizinhoguata@gmail.com>
2021-04-01 21:18:50 -0300

    Atividade_1

---
+++ script.js
@@ -0,0 +1,14 @@
+function subtrairValores(){
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var n3 = parseInt(n1) - parseInt(n2);
+    alert(n3);
+ } 
+ 
+ function somarValores(){
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var n3 = parseInt(n1) + parseInt(n2);
+    alert(n3);
+     }
+
